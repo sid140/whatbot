8920b6fdd797a86166ca50b56ea4fbce309c9fb2
Siddex <920976+sid140@users.noreply.github.com>
2023-07-03 15:28:12 +0530

    Add files via upload
    Updated

--- Whatbot.py
+++ Whatbot.py
@@ -2,6 +2,19 @@ import pyautogui as rub
 import webbrowser as kiss
 import time as good
 
+jimmy = '''
+I8,        8        ,8I 88        88        db   888888888888 88888888ba    ,ad8888ba, 888888888888
+`8b       d8b       d8' 88        88       d88b       88      88      "8b  d8"'    `"8b     88
+ "8,     ,8"8,     ,8"  88        88      d8'`8b      88      88      ,8P d8'        `8b    88
+  Y8     8P Y8     8P   88aaaaaaaa88     d8'  `8b     88      88aaaaaa8P' 88          88    88
+  `8b   d8' `8b   d8'   88""""""""88    d8YaaaaY8b    88      88""""""8b, 88          88    88
+   `8a a8'   `8a a8'    88        88   d8""""""""8b   88      88      `8b Y8,        ,8P    88
+    `8a8'     `8a8'     88        88  d8'        `8b  88      88      a8P  Y8a.    .a8P     88
+     `8'       `8'      88        88 d8'          `8b 88      88888888P"    `"Y8888Y"'      88
+'''
+
+print(jimmy)
+
 while True:
     cold = rub.alert(text='Welcome to Whatbot, a Whatsapp bot created SJK', title='Circus', button='OK')
     print("Selfish advertisement: This bot is developed by Sid\n")
