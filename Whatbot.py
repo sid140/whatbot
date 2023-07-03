1efe1c27c4a9bfd9c62400cc92a38bac5b975cbb
Siddex <920976+sid140@users.noreply.github.com>
2023-07-03 15:05:07 +0530

    Updated

--- Whatbot.py
+++ Whatbot.py
@@ -2,28 +2,28 @@ import pyautogui as rub
 import webbrowser as kiss
 import time as good
 
-cold = rub.alert(text='Welcome to Whatbot, a Whatsapp bot created SJK', title='Circus', button='OK')
-print("Selfish advertisement : This bot is developed by Sid\n")
-runner= input ('Enter the Whatsapp name:')
-lind = input("Enter the fist link: ")
-msg = input("Enter the message: \n")
-if cold == 'OK':
-    rub.press('win', interval = 0.2)
-    rub.write('Whatsapp', interval = 0.2)
-    rub.press('enter',interval=0.2)
-    good.sleep(3)
-    rub.write(runner,interval=0.4)
-    rub.press('tab',interval=0.4)
-    rub.press('enter',interval=0.3)
-    rub.write(lind,interval=0.4)
-    rub.press(msg,interval=0.2)
-    rub.press('enter',interval=0.2)
-    good.sleep(3)
-    rub.write(msg,interval=0.2)
-    rub.press('enter',interval=0.2)
+while True:
+    cold = rub.alert(text='Welcome to Whatbot, a Whatsapp bot created SJK', title='Circus', button='OK')
+    print("Selfish advertisement: This bot is developed by Sid\n")
+    runner = input('Enter the Whatsapp name: ')
+    lind = input("Enter the first link: ")
+    msg = input("Enter the message: \n")
     
-    
+    if cold == 'OK':
+        rub.press('win', interval=0.2)
+        rub.write('Whatsapp', interval=0.2)
+        rub.press('enter', interval=0.2)
+        good.sleep(3)
+        rub.write(runner, interval=0.4)
+        rub.press('tab', interval=0.4)
+        rub.press('enter', interval=0.3)
+        rub.write(lind, interval=0.4)
+        rub.press(msg, interval=0.2)
+        rub.press('enter', interval=0.2)
+        good.sleep(3)
+        rub.write(msg, interval=0.2)
+        rub.press('enter', interval=0.2)
 
-    
-
-
+    repeat = input("Do you want to send another message? (yes/no): ")
+    if repeat.lower() == 'no':
+        break
